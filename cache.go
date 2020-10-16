3196a0f0420cee24bdf8d56508bb98798228abd5
petechen <15736755067@163.com>
2020-10-16 13:30:01 +0800

    update

--- cache.go
+++ cache.go
@@ -14,9 +14,29 @@ type Cache struct {
 	WriteTime int64
 }
 
+type Add struct {
+	Key interface{}
+	Val interface{}
+}
+
 func init() {
 	Timeout = 60
 	deleteCache()
+}
+
+func AddData(params []*Add) {
+	wg := sync.WaitGroup{}
+	wg.Add(len(params))
+	for i := 0; i < len(params); i++ {
+		go func(v *Add) {
+			DataCache.Store(v.Key, &Cache {
+				Data:      v.Val,
+				WriteTime: time.Now().Unix(),
+			})
+		}(params[i])
+		wg.Done()
+	}
+	wg.Wait()
 }
 
 func deleteCache() {
